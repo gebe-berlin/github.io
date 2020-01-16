282aa85e6617a363c05dc137f3e8c92449012c81
gebe-berlin <59953921+gebe-berlin@users.noreply.github.com>
2020-01-16 11:40:11 +0100

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,1 @@
-document.getElementById('date').innerHTML = new Date().toDateString();
+document.getElementById('date').innerHTML = new Date().toDateString().concat("\r\nThis is about RPA, but in JS");
